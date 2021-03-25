37bf8f026f795f6906fdcfecbc0b4bc68512680d
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-03-25 16:16:48 -0400

    added a function getUserCoordinatesFromCityName(cityName), object satteliteCategories that contains all sattelite categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,66 @@
 
+
+//This object will be displayed on UI as a Select option for users to choose
+const satteliteCategories = {
+    'Amateur radis': 18,
+    'Beidou Navigation System': 35,
+    'Brightest': 1,
+    'Celestis': 45,
+    'CubeSats': 32,
+    'Disaster monitoring': 8,
+    'Earth resources': 6,
+    'Education': 29,
+    'Engineering': 28,
+    'Experimental': 19,
+    'Flock': 48,
+    'Galileo': 22,
+    'Geodetic': 27,
+    'Geostationary': 10,
+    'Global Positioning System (GPS) Constellation': 50,
+    'Global Positioning System (GPS) Operational': 20,
+    'Globalstar': 17,
+    'Glonass Constellation': 51,
+    'Glonass Operational': 21,
+    'GOES': 5,
+    'Gonets': 40,
+    'Gorizont': 12,
+    'Intelsat': 11,
+    'Iridium': 15,
+    'IRNSS': 46,
+    'ISS': 2,
+    'Lemur': 49,
+    'Military': 30,
+    'Molniya': 14,
+    'Navy Navigation Satellite System': 24,
+    'NOAA': 4,
+    'O3B Networks': 43,
+    'OneWeb': 53,
+    'Orbcomm': 16,
+    'Parus': 38,
+    'QZSS': 47,
+    'Radar Calibration': 31,
+    'Raduga': 13,
+    'Russian LEO Navigation': 25,
+    'Satellite-Based Augmentation System': 23,
+    'Search & rescue': 7,
+    'Space & Earth Science': 26,
+    'Starlink': 52,
+    'Strela': 39,
+    'Tracking and Data Relay Satellite System': 9,
+    'Tselina': 44,
+    'Tsikada': 42,
+    'Tsiklon': 41,
+    'TV': 34,
+    'Weather': 3,
+    'Westford Needles': 37,
+    'XM and Sirius': 33,
+    'Yaogan': 36
+}
+
+//Object.keys(satteliteCategories).forEach( a=> console.log(a))
+
 //getSattelitesNearMe(38.846226, -77.306374, 0, 45, 30);
+getUserCoordinatesFromCityName('Fairfax');
 
 
 /**
@@ -8,7 +69,7 @@
  * @var lat is lattitude of the observer
  * @var alt is altitide of the observer
  * @var searchRadius is between 0 and 90 degrees above the observer to find sattelites
- * @var categoryID will come from the list of all categories (total 53)
+ * @var categoryID will come from the list of all categories (total 53, variable = satteliteCategories)
  * @todo add note regarding how to handle CORS(Cross-Origin Resource Sharing) in README.md file
  */
 function getSattelitesNearMe(lat, lng, alt, searchRadius, categoryID) {
@@ -22,6 +83,26 @@ function getSattelitesNearMe(lat, lng, alt, searchRadius, categoryID) {
             }
             //following code will be changed with actual implementation...
             console.log(response.json());
+        })
+        .catch(function (error) {
+            console.log('Exception caught with an error: \n', error);
+        })
+}
+
+/**
+ * Function will get coordinates of given city name
+ * @param cityName is a String name of the city user wants to get sattelite above.
+ * @todo lat and lng is printed on console for now, will be used  as a variable later.
+ */
+function getUserCoordinatesFromCityName(cityName) {
+    let baseURL = 'https://api.openweathermap.org';
+    let endPoint = `${baseURL}/data/2.5/weather?q=${cityName}&appid=6eff42fd74f00dfa17ce2ae0939485b8`;
+
+    fetch(endPoint)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.coord.lat);
+            console.log(data.coord.lon);
         })
         .catch(function (error) {
             console.log('Exception caught with an error: \n', error);
