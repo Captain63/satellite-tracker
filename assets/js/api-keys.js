f2cbc5b0f7e058ca65b4f0c7fb6cb919902d3925
Captain63 <Captain63@users.noreply.github.com>
2021-04-03 12:07:25 -0400

    Create separate file for api-keys hosting (linked in html head)

---
+++ assets/js/api-keys.js
@@ -0,0 +1,5 @@
+const googleMapsAPI = `[YOUR KEY HERE]`;
+const n2yoAPI = `[YOUR KEY HERE]`;
+
+// Sets Google Maps API key for script link
+document.querySelector("#maps-link").setAttribute("src", `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPI}&libraries=&v=weekly`);
